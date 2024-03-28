4c7ef9cfed008090a979923d6c416c1e89cfb757
Lin Htet <earthangle007@gmail.com>
2024-03-28 17:35:40 +0800

    Modify DailyMedicationManagerTest to test sublist printing

--- src/test/java/meditracker/DailyMedicationManagerTest.java
+++ src/test/java/meditracker/DailyMedicationManagerTest.java
@@ -1,6 +1,13 @@
 package meditracker;
 
+import meditracker.command.AddCommand;
+import meditracker.exception.ArgumentNotFoundException;
+import meditracker.medication.Medication;
+import meditracker.medication.MedicationManager;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -9,16 +16,44 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DailyMedicationManagerTest {
     @Test
-    public void addDailyMedication_genericDailyMedication_dailyMedicationAdded() {
-        DailyMedicationManager.clearDailyMedication();
-        DailyMedication dailyMedication = new DailyMedication("TestMedication");
-        DailyMedicationManager.addDailyMedication(dailyMedication);
+    public void addDailyMedication_genericDailyMedication_dailyMedicationAdded() throws ArgumentNotFoundException {
+        String inputString = "add -n Medication_A -q 60.0 -d 500.0 -e 01/07/25 -f morning -dM 500.0 -dA 250.0 "
+                + "-dE 300.0 -r cause_dizziness -rep 1";
+        MedicationManager medicationManager = new MedicationManager();
+        AddCommand command = new AddCommand(inputString);
+        command.execute(medicationManager);
 
-        DailyMedicationManager.printMedications();
+        List<DailyMedication> morningMedications = new ArrayList<>();
+        List<DailyMedication> afternoonMedications = new ArrayList<>();
+        List<DailyMedication> eveningMedications = new ArrayList<>();
+
+        DailyMedication morningMeds = new DailyMedication("Medication_A");
+        morningMedications.add(morningMeds);
+
+        DailyMedication afternoonMeds = new DailyMedication("Medication_A");
+        afternoonMedications.add(afternoonMeds);
+
+        DailyMedication eveningMeds = new DailyMedication("Medication_A");
+        eveningMedications.add(eveningMeds);
+
+
+        List<Medication> medicationList = medicationManager.getMedications();
+
+        DailyMedicationManager.printTodayMedications(medicationList,
+                morningMedications, "Morning:");
+        DailyMedicationManager.printTodayMedications(medicationList,
+                afternoonMedications, "Afternoon:");
+        DailyMedicationManager.printTodayMedications(medicationList,
+                eveningMedications, "Evening:");
 
         int actualIndex = 1; // 1-based indexing
-        DailyMedication dailyMedicationTest = DailyMedicationManager.getDailyMedication(actualIndex);
-        assertEquals(dailyMedication.toString(), dailyMedicationTest.toString());
+        DailyMedication morningMedicationTest = DailyMedicationManager.getMorningMedication(actualIndex);
+        DailyMedication afternoonMedicationTest = DailyMedicationManager.getAfternoonMedication(actualIndex);
+        DailyMedication eveningMedicationTest = DailyMedicationManager.getEveningMedication(actualIndex);
+
+        assertEquals(morningMeds.toString(), morningMedicationTest.toString());
+        assertEquals(afternoonMeds.toString(), afternoonMedicationTest.toString());
+        assertEquals(eveningMeds.toString(), eveningMedicationTest.toString());
     }
 
     @Test
