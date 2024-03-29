4deb0e6d086ac0b942b7b6c9cceaac0289fd8a4f
E0958902 <E0958902@u.nus.edu>
2024-03-29 11:58:15 +0800

    Remove getDosage and getIntakeFreq

--- src/main/java/meditracker/medication/MedicationManager.java
+++ src/main/java/meditracker/medication/MedicationManager.java
@@ -80,7 +80,7 @@ public class MedicationManager {
     public void printAllMedications() {
         assert medications != null;
         System.out.println("You have " + getTotalMedications() + " medications listed below.");
-        System.out.println("\t" + "Format: Name | Quantity | Dosage | Expiry Date | Remarks");
+        System.out.println("Format: Name | Quantity | Expiry Date | Remarks");
         Ui.printMedsList(medications);
     }
 }
