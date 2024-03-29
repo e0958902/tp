9d832b3c2abc7495f2071bd5c1e9a66aecbf3f62
E0958902 <E0958902@u.nus.edu>
2024-03-29 11:47:45 +0800

    Update ListCommandTest to only import required assertion

--- src/test/java/meditracker/command/ListCommandTest.java
+++ src/test/java/meditracker/command/ListCommandTest.java
@@ -4,7 +4,7 @@ import meditracker.medication.Medication;
 import meditracker.medication.MedicationManager;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ListCommandTest {
 
