aa313e39980181ca56e5651f1ad83c27f51cdad3
E0958902 <E0958902@u.nus.edu>
2024-03-30 01:36:39 +0800

    Update AddCommand JUnit test to use static MedicationManager

--- src/test/java/meditracker/AddCommandTest.java
+++ src/test/java/meditracker/AddCommandTest.java
@@ -5,11 +5,24 @@ import meditracker.exception.ArgumentNotFoundException;
 import meditracker.exception.DuplicateArgumentFoundException;
 import meditracker.exception.HelpInvokedException;
 import meditracker.medication.MedicationManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddCommandTest {
+
+    @BeforeEach
+    public void resetMedicationManager() throws InvocationTargetException,
+            IllegalAccessException, NoSuchMethodException {
+        Method resetMedicationManagerMethod
+                = MedicationManager.class.getDeclaredMethod("clearMedication");
+        resetMedicationManagerMethod.setAccessible(true);
+        resetMedicationManagerMethod.invoke(MedicationManager.class);
+    }
 
     // 3 part format
     // methodBeingTested_conditionToTest_expectedOutcome
@@ -19,11 +32,10 @@ class AddCommandTest {
         // setup lines
         String inputString = "add -n Medication_A -q 60.0 -d 500.0 -e 01/07/25 -f morning -dM 500.0 -dA 250.0 "
                 + "-r cause_dizziness -rep 1";
-        MedicationManager medicationManager = new MedicationManager();
         AddCommand command = new AddCommand(inputString);
-        command.execute(medicationManager);
+        command.execute();
 
         // actual test
-        assertEquals(1, medicationManager.getTotalMedications());
+        assertEquals(1, MedicationManager.getTotalMedications());
     }
 }
