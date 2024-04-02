aeb2248ddbf19aae75aa040755588a668eb09ba4
annoy-o-mus <55198236+annoy-o-mus@users.noreply.github.com>
2024-04-03 01:39:00 +0800

    style: Fix unused imports

--- src/test/java/meditracker/storage/FilePathCheckerTest.java
+++ src/test/java/meditracker/storage/FilePathCheckerTest.java
@@ -1,7 +1,6 @@
 package meditracker.storage;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
