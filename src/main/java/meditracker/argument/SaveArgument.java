a3e18c30bf9fc8a9cd79421b0f5430efc928e653
annoy-o-mus <55198236+annoy-o-mus@users.noreply.github.com>
2024-04-13 10:37:10 +0800

    doc: Add javadoc comments

--- src/main/java/meditracker/argument/SaveArgument.java
+++ src/main/java/meditracker/argument/SaveArgument.java
@@ -1,7 +1,12 @@
 package meditracker.argument;
 
+/**
+ * Contains the attributes of the save argument of the associated `save` command.
+ */
 public class SaveArgument extends Argument {
-
+    /**
+     * Sets up the attributes of the save argument of the associated `save` command.
+     */
     public SaveArgument() {
         super(
                 ArgumentName.SAVE_FILE,
