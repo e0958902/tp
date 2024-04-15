7ec7c5f252cfe4cdbc6587de8abcf9e8064243d0
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-04-15 19:58:09 +0800

    fix: Fix style again

--- src/main/java/meditracker/time/TimeRange.java
+++ src/main/java/meditracker/time/TimeRange.java
@@ -3,17 +3,17 @@ package meditracker.time;
 import java.time.LocalTime;
 
 /**
- * Manages a start and end time, to maintain information on a time range
+ * Manages a start and end time, to maintain information on a time range.
  */
 public class TimeRange {
     private final LocalTime start;
     private final LocalTime end;
 
     /**
-     * Constructs a TimeRange using the start and end time
+     * Constructs a TimeRange using the start and end time.
      *
-     * @param start Start time of TimeRange
-     * @param end End time of TimeRange
+     * @param start Start time of TimeRange.
+     * @param end End time of TimeRange.
      */
     public TimeRange(LocalTime start, LocalTime end) {
         this.start = start;
@@ -21,10 +21,10 @@ public class TimeRange {
     }
 
     /**
-     * Checks whether a given time is within the time range
+     * Checks whether a given time is within the time range.
      *
-     * @param time Given time to check the range
-     * @return True if it is within the time range, else False
+     * @param time Given time to check the range.
+     * @return True if it is within the time range, else False.
      */
     public boolean isWithinTimeRange(LocalTime time) {
         boolean isWithinStartAndEnd = time.isAfter(start) && time.isBefore(end);
