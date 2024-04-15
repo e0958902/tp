51ca5e5e0ddea309673823b515d3191beeea9fd4
E0958902 <E0958902@u.nus.edu>
2024-04-15 10:31:51 +0800

    style: fix style in MedicationManagerTest

--- src/test/java/meditracker/medication/MedicationManagerTest.java
+++ src/test/java/meditracker/medication/MedicationManagerTest.java
@@ -3,13 +3,24 @@ package meditracker.medication;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+/**
+ * This test file is to perform tests on the MedicationManager
+ */
+
 public class MedicationManagerTest {
+
+    /**
+     * Resets the MedicationManager
+     *
+     * @throws InvocationTargetException when it fails to call the reset method
+     * @throws IllegalAccessException when the method can not access the specified class
+     * @throws NoSuchMethodException
+     */
 
     // @@author e0958902
     public static void resetMedicationManager() throws InvocationTargetException,
             IllegalAccessException, NoSuchMethodException {
-        Method resetMedicationManagerMethod
-                = MedicationManager.class.getDeclaredMethod("clearMedication");
+        Method resetMedicationManagerMethod = MedicationManager.class.getDeclaredMethod("clearMedication");
         resetMedicationManagerMethod.setAccessible(true);
         resetMedicationManagerMethod.invoke(MedicationManager.class);
     }
